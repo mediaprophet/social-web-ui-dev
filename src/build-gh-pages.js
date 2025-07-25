1d56c96a1162e39624bbe7f3b6fcf1aeefc180cb
mediaprophet <34872169+mediaprophet@users.noreply.github.com>
2025-07-26 06:05:07 +1000

    Update build-gh-pages.js

--- src/build-gh-pages.js
+++ src/build-gh-pages.js
@@ -11,7 +11,7 @@ const assetsDest = path.join(docsDir, 'assets');
 // 1. Build Tailwind CSS
 const { execSync } = require('child_process');
 console.log('Building Tailwind CSS...');
-execSync('npx tailwindcss -i ./src/tailwind.css -o ../docs/style.css --minify', { stdio: 'inherit' });
+execSync('npx tailwindcss -i ./src/tailwind.css -o ../docs/dist/style.css --minify', { stdio: 'inherit' });
 
 // 2. Copy HTML files and update CSS link
 console.log('Copying HTML files...');
@@ -20,7 +20,8 @@ glob.sync('**/*.html', { cwd: srcPieces }).forEach(file => {
   const destFile = path.join(docsDir, file);
   let html = fs.readFileSync(srcFile, 'utf8');
   // Replace CSS link
-  html = html.replace(/<link rel="stylesheet" href="[^"]*style\.css"/g, '<link rel="stylesheet" href="style.css"');
+  html = html.replace(/<link rel="stylesheet" href="[^"]*style\.css"/g, '<link rel="stylesheet" href="dist/style.css"');
+  html = html.replace(/src\/(assets\/[^"']+)/g, 'https://mediaprophet.github.io/social-web-ui-dev/$1');
   fs.ensureDirSync(path.dirname(destFile));
   fs.writeFileSync(destFile, html);
 });
